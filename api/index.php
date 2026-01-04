fbe6dd5152f66c9a4ac0171168ad252771f1a883
jmu809 <jrmu700@gmail.com>
2026-01-04 19:06:14 +0100

    prueba

--- api/index.php
+++ api/index.php
@@ -31,7 +31,9 @@
       <tbody>
         <?php
 
-        $conexion = mysqli_connect(getenv('MYSQL_HOST'), getenv('MYSQL_USER'), getenv('MYSQL_PASSWORD'), "SG");
+        /*$conexion = mysqli_connect(getenv('MYSQL_HOST'), getenv('MYSQL_USER'), getenv('MYSQL_PASSWORD'), "SG");
+         */
+        $conexion = mysqli_connect("34.77.160.11", "root", "Root.123", "SG");
 
         $cadenaSQL = "select * from s_customer";
         $resultado = mysqli_query($conexion, $cadenaSQL);
